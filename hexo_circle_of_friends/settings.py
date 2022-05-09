150eedc9deb4dfd62c8edb006c4674f0e39edadf
Zkeq <learnonly@learnonly.xyz>
2022-05-09 08:02:03 +0800

    Update settings.py

--- hexo_circle_of_friends/settings.py
+++ hexo_circle_of_friends/settings.py
@@ -17,7 +17,7 @@
 # 支持配置多个友链页面并指定不同主题策略，每个用{}分隔，它们会被同时爬取，数据保存在一起。***至少配置一个***
 LINK = [
     {
-        "link": "https://zfe.space/link/",  # 友链页地址1，修改为你的友链页地址
+        "link": "https://icodeq.com/link/",  # 友链页地址1，修改为你的友链页地址
         "theme": "butterfly"
     },
     #     {
@@ -43,14 +43,14 @@ SETTINGS_FRIENDS_LINKS = {
     "enable": False,
     "json_api": "",
     "list": [
-        # 示例1：
-        ["贰猹の小窝", "https://noionion.top/", "https://pub-noionion.oss-cn-hangzhou.aliyuncs.com/head.jpg"],
-        ["Akilarの糖果屋", "https://akilar.top/", "https://akilar.top/images/headimage.png"],
-        # 示例2：使用suffix的配置如下
-        # 程序目前自动获取 "atom.xml", "feed/atom", "rss.xml", "rss2.xml", "feed", "index.xml" 这些后缀
-        # 如果目标地址的后缀不在其中，就可以在这里添加
-        # 比如 https://elizen.me/hello.xml ，填写：
-        ["elizen", "https://elizen.me/", "https://akilar.top/images/headimage.png", "hello.xml"],
+#         # 示例1：
+#         ["贰猹の小窝", "https://noionion.top/", "https://pub-noionion.oss-cn-hangzhou.aliyuncs.com/head.jpg"],
+#         ["Akilarの糖果屋", "https://akilar.top/", "https://akilar.top/images/headimage.png"],
+#         # 示例2：使用suffix的配置如下
+#         # 程序目前自动获取 "atom.xml", "feed/atom", "rss.xml", "rss2.xml", "feed", "index.xml" 这些后缀
+#         # 如果目标地址的后缀不在其中，就可以在这里添加
+#         # 比如 https://elizen.me/hello.xml ，填写：
+#         ["elizen", "https://elizen.me/", "https://akilar.top/images/headimage.png", "hello.xml"],
     ]
 }
 
